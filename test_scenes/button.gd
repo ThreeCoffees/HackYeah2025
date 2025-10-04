c5fcee24d7ab4ae9262652739b898b3c689088d4
gre-v-el <myszkier.gabriel@gmail.com>
2025-10-04 15:18:12 +0200

    Added audio manager and a flick sound when clicking a button

--- test_scenes/button.gd
+++ test_scenes/button.gd
@@ -17,5 +17,6 @@ func _process(delta: float) -> void:
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	var mouse_click = event as InputEventMouseButton
 	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
+		AudioManager.play("flick")
 		pressed.emit()
 	pass # Replace with function body.
